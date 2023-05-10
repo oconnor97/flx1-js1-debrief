0d6d37d928c75d20f44627bd3c4afb33d53bcb6e
Juliza <zepeda.juliza@gmail.com>
2023-05-10 17:46:37 -0400

    removed comments

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// let myNumber = 30;
-// console.log(typeof myNumber)
-// if(typeof myNumber !== 'number'){
-//     console.log('This is not a number')
-// } else if(myNumber % 3 === 0 && myNumber % 5 === 0){
-//     console.log('Genbuzz');
-// } else if(myNumber % 3 === 0){
-//     console.log('Gen');
-// } else if(myNumber % 5 === 0){
-//     console.log('Buzz');
-// } else {
-//     console.log(myNumber);
-// }
+let myNumber = 30;
+console.log(typeof myNumber)
+if(typeof myNumber !== 'number'){
+    console.log('This is not a number')
+} else if(myNumber % 3 === 0 && myNumber % 5 === 0){
+    console.log('Genbuzz');
+} else if(myNumber % 3 === 0){
+    console.log('Gen');
+} else if(myNumber % 5 === 0){
+    console.log('Buzz');
+} else {
+    console.log(myNumber);
+}
 
 let item = 'socks';
 
